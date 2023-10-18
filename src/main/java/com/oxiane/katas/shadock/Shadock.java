759254d15e7f6af62a8cfc1e0a4a53f6dff9f4eb
Christophe Marchand <cmarchand@clever-age.com>
2023-10-18 12:20:23 +0200

    With only an array

--- src/main/java/com/oxiane/katas/shadock/Shadock.java
+++ src/main/java/com/oxiane/katas/shadock/Shadock.java
@@ -1,43 +1,15 @@
 package com.oxiane.katas.shadock;
 
-import java.util.Arrays;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 public class Shadock {
+  private static final String[] WORDS = {"GA", "BU", "ZO", "MEU"};
   public String toShadock(int input) {
     String numberBase4 = Integer.toString(input, 4);
     return numberBase4.chars()
-        .mapToObj(value -> Character.toString(value))
-        .map(s -> ShadockStrategy.of(s))
-        .map(sh -> sh.word())
+        .map(i -> i - '0')
+        .mapToObj(i -> WORDS[i])
         .collect(Collectors.joining());
   }
 
-  private enum ShadockStrategy {
-    ZERO("0", () -> "GA"),
-    ONE("1", () -> "BU"),
-    TWO("2", () -> "ZO"),
-    THREE("3", () -> "MEU")
-    ;
-
-    private final Supplier<String> supplier;
-    private final String digit;
-
-    ShadockStrategy(String digit, Supplier<String> supplier) {
-      this.digit = digit;
-      this.supplier = supplier;
-    }
-
-    public static ShadockStrategy of(String s) {
-      return Arrays.stream(values())
-          .filter(shadockStrategy -> shadockStrategy.digit.equals(s))
-          .findFirst()
-          .orElseThrow();
-    }
-
-    public String word() {
-      return supplier.get();
-    }
-  }
 }
